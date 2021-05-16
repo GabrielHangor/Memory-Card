dec98cb3fab0e567693f251ad22fdefb70cc83a8
HangorG <gaba93@yandex.ru>
2021-05-16 20:52:22 +0300

    set cardsArray as an initial state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Main from './main/Main';
 import Footer from './Footer';
+import cards from './CardsArray';
 
 function App() {
+  const [cardsArray, setCardsAttay] = useState();
+
+  useEffect(() => {
+    setCardsAttay(cards);
+  }, []);
+
+  console.log(cardsArray)
+
   return (
     <div className="container">
       <Header />
---
+++ src/CardsArray.js
@@ -0,0 +1,63 @@
+const cardsArray = [
+  {
+    id: 0,
+    src: '/memory-card/src/images/Renji.png',
+    name: 'Renji Abarai',
+    clicked: false,
+  },
+  {
+    id: 1,
+    src: '/memory-card/src/images/Aizen.png',
+    name: 'Sōsuke Aizen',
+    clicked: false,
+  },
+  {
+    id: 2,
+    src: '/memory-card/src/images/Gin.png',
+    name: 'Gin Ichimaru',
+    clicked: false,
+  },
+  {
+    id: 3,
+    src: '/memory-card/src/images/Komamura.png',
+    name: 'Sajin Komamura',
+    clicked: false,
+  },
+  {
+    id: 4,
+    src: '/memory-card/src/images/Rukia.png',
+    name: 'Rukia Kuchiki',
+    clicked: false,
+  },
+  {
+    id: 5,
+    src: '/memory-card/src/images/Ikkaku.png',
+    name: 'Ikkaku Madarame',
+    clicked: false,
+  },
+  {
+    id: 6,
+    src: '/memory-card/src/images/Yoruichi.png',
+    name: 'Yoruichi Shihōin',
+    clicked: false,
+  },
+  {
+    id: 7,
+    src: '/memory-card/src/images/Kenpachi.png',
+    name: 'Kenpachi Zaraki',
+    clicked: false,
+  },
+  {
+    id: 8,
+    src: '/memory-card/src/images/Ichigo.png',
+    name: 'Ichigo Kurosaki',
+    clicked: false,
+  },
+  {
+    src: '/memory-card/src/images/Harribel.png',
+    name: 'Tier Harribel',
+    clicked: false,
+  },
+];
+
+export default cardsArray;
